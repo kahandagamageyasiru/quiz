ff95a4728291cbf53b982653cabda2f9a0401b98
kahandagamageyasiru <107664991+kahandagamageyasiru@users.noreply.github.com>
2023-01-13 21:47:57 +0530

    Add files via upload

---
+++ SdamQuizSystem/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SdamQuizSystem
+{
+    class Question
+    {
+        public string Questions { get; set; }
+
+        public string[] Answer { get; set; }
+        public int CorrectAnswerIndex { get; set; }
+        public Question(string questions, string[] answer, int correctAnswerIndex)
+        {
+            Questions = questions;
+            Answer = answer;
+            CorrectAnswerIndex = correctAnswerIndex;
+        }
+    }
+}
